7861193e457c914c07b8d2988afdddc03c31242f
Aaditya Khurrana <90464753+AadityaKhurrana@users.noreply.github.com>
2023-01-24 17:01:05 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
-let speed = 4;
+let speed = 5;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -45,16 +45,22 @@ function gameEngine()
         gameOverSound.play();
         musicSound.pause();
         inputDir =  {x: 0, y: 0}; 
+        score = 0; 
+        document.getElementById("score").innerHTML=`Score:${score}`;
         alert("Game Over. Press any key to play again!");
         snakeArr = [{x: 13, y: 15}];
         musicSound.play();
-        score = 0; 
+        
     }
     //when snake eat the food generate the random food
     if(snakeArr[0].x==food.x && snakeArr[0].y==food.y)
     {
         foodSound.play()
         score++;
+        if(score%10===0)
+        {
+            speed*=1.5;
+        }
         document.getElementById("score").innerHTML=`Score:${score}`;
         snakeArr.unshift({x:snakeArr[0].x+inputDir.x , y:snakeArr[0].y+inputDir.y});
         let a=2 , b=16 ;
